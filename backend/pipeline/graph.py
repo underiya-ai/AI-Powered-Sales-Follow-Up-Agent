3f73e7c42cf9e762b86d5846846f8861e6ffc346
Aniket Underiya <aniketunderiya20@gmail.com>
2026-08-29 22:58:34 +0530

    add node of next_action_agent in graph

--- backend/pipeline/graph.py
+++ backend/pipeline/graph.py
@@ -4,6 +4,7 @@ from backend.pipeline.state import SalesState
 from backend.Agents.sales_orchestrator import sales_orchestrator
 from backend.Agents.conversation_intelligence import conversation_intelligence
 from backend.Agents.lead_scoring_agent import lead_scoring_agent
+from backend.Agents.next_best_action_agent import next_best_action_agent
 
 
 def create_sales_graph():
@@ -13,6 +14,7 @@ def create_sales_graph():
     graph.add_node("sales_orchestrator",sales_orchestrator)
     graph.add_node("conversation_intelligence",conversation_intelligence)
     graph.add_node("lead_scoring",lead_scoring_agent)
+    graph.add_node("next_action_agent",next_best_action_agent)
 
     # Agents will be added here
     # graph.add_node("conversation_intelligence", ...)
@@ -25,7 +27,8 @@ def create_sales_graph():
     graph.add_edge(START, "sales_orchestrator")
     graph.add_edge("sales_orchestrator", "conversation_intelligence")  
     graph.add_edge("conversation_intelligence","lead_scoring")
-    graph.add_edge("lead_scoring",END)
+    graph.add_edge("lead_scoring","next_action_agent")
+    graph.add_edge("next_action_agent",END)
 
     return graph.compile()
 
